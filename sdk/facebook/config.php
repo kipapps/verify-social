46f537604967b32d86c58414063221536e70b37c
Kip <ki2thap@gmail.com>
2017-03-21 18:55:19 -0400

    Initial

---
+++ sdk/facebook/config.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('fb_appId', '119093284798281');
+define('fb_secret', '681a0c2ddfd8a329420b39ebc27a6713');
+define('OAUTH_CALLBACK', 'http://kipbeats.com/flremakes/callback.php');
+
