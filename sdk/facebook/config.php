a5946295680626bc3860f8789d7417c999096aa1
kipapps <ki2thap@gmail.com>
2017-03-21 19:48:14 -0400

    Update config.php

--- sdk/facebook/config.php
+++ sdk/facebook/config.php
@@ -5,7 +5,6 @@
  * A single location to store configuration.
  */
 
-define('fb_appId', '119093284798281');
-define('fb_secret', '681a0c2ddfd8a329420b39ebc27a6713');
+
 define('OAUTH_CALLBACK', 'http://kipbeats.com/flremakes/callback.php');
 
