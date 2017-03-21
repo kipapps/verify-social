5c5e17d05659276e7671399c62127de5f5e72eb4
kipapps <ki2thap@gmail.com>
2017-03-21 19:39:26 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: Verify Social
+Plugin URI: http://kipsoft.com
+Description: Make sure a user is liking subscribed or following you before viewing certain content.
+Author: KipSoft
+Version: 1.0
+Author URI: http://kipsoft.com
+*/
+
+//print_r(__DIR__);exit;
+//require_once(ABSPATH.'wp-includes/pluggable.php');
+
+
+phpinfo();
+
+
+
+?>
